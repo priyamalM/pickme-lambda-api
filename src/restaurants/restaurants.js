190cd71541dc37bda31e9c1aa3b8e5d3fde13e3f
priyamalM <madushanka.priyamal@yahoo.com>
2023-02-05 13:10:18 +0530

    added delete for restaurants

--- src/restaurants/restaurants.js
+++ src/restaurants/restaurants.js
@@ -18,54 +18,65 @@ const dynamo = DynamoDBDocumentClient.from(client);
 const tableName = "Restaurant";
 
 module.exports.handler = async (event) => {
-    let body;
-    let statusCode = 200;
-    const headers = {
-        "Content-Type": "application/json",
-    };
-    try {
-        switch (event.httpMethod) {
-            case 'POST':
-                let requestJSON = JSON.parse(event.body);
-                await dynamo.send(
-                  new PutCommand({
-                    TableName: tableName,
-                    Item: {
-                      restaurant_id: requestJSON.id,
-                      location: requestJSON.town,
-                      name: requestJSON.name,
-                    },
-                  })
-                );
-                body = requestJSON;
-                break;
-            case 'GET':
-                body = await dynamo.send(
-                  new GetCommand({
-                    TableName: tableName,
-                    Key: {
-                      restaurant_id: event.pathParameters.id,
-                    },
-                  })
-                  );
-                  body = body.Item;
-                  break;
-            default:
-                throw new Error(`Unsupported route: "${event.routeKey}"`);        
-        }
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  try {
+    switch (event.httpMethod) {
+      case 'POST':
+        let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              restaurant_id: requestJSON.id,
+              location: requestJSON.town,
+              name: requestJSON.name,
+            },
+          })
+        );
+        body = requestJSON;
+        break;
+      case 'GET':
+        body = await dynamo.send(
+          new GetCommand({
+            TableName: tableName,
+            Key: {
+              restaurant_id: event.pathParameters.id,
+            },
+          })
+        );
+        body = body.Item;
+        break;
+      case 'DELETE':
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              restaurant_id: event.pathParameters.id,
+            },
+          })
+        );
+        body = {};
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
     }
-    catch (error) {
-        const message = error.message ? error.message : 'Internal server error'
-        statusCode = 400;
-        body = message;
-        return sendResponse(500, { message })
-    } finally {
-        body = JSON.stringify(body);
-    }
+  }
+  catch (error) {
+    const message = error.message ? error.message : 'Internal server error'
+    statusCode = 400;
+    body = message;
+    return sendResponse(500, { message })
+  } finally {
+    body = JSON.stringify(body);
+  }
 
-    return {
-        statusCode,
-        body,
-        headers,
-      };
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 }
